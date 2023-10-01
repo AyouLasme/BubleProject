488163bb573e32dd9b7503d1e19f5691d175eb63
adieme <aissatou-combe-a.dieme@etudiant.univ-rennes1.fr>
2023-10-01 22:24:14 +0200

    Implementation du Bubble Graph

---
+++ src/components/BubbleChart.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+
+class BubbleChart extends Component {
+  state = {
+    data: null,
+    isBubbleChartVisible: false,
+  };
+
+  svgRef = React.createRef();
+  width = 800;
+  height = 600;
+  treeLayout = d3.tree().size([this.width, this.height]);
+  marginX = 50;
+  marginY = 50;
+
+  telechargerFichier = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      try {
+        this.setState({ data: JSON.parse(e.target.result) });
+      } catch (error) {
+        console.error('Erreur lors de la lecture du fichier JSON :', error);
+      }
+    };
+    reader.readAsText(file);
+  };
+
+  createBubbleChart = () => {
+    // Supprimez le contenu SVG existant s'il y en a un
+    d3.select(this.svgRef.current).selectAll('*').remove();
+
+    const { data } = this.state;
+    const svg = d3.select(this.svgRef.current).attr('width', this.width).attr('height', this.height);
+    const root = d3.hierarchy(data);
+    const treeData = this.treeLayout(root);
+
+    treeData.descendants().forEach((node) => {
+      // Ajoutez les marges aux coordonnées x et y
+      node.x += this.marginX;
+      node.y = node.y * 0.5 + this.marginY; // Pour réduire la longueur des liens
+    });
+
+    const node = svg
+      .selectAll('.node')
+      .data(treeData.descendants())
+      .enter()
+      .append('g')
+      .attr('class', 'node')
+      .attr('transform', (d) => `translate(${d.x},${d.y})`)
+      .on('click', this.handleBubbleClick);
+
+    node
+      .append('circle')
+      .attr('r', (d) => Math.max(15, d.data.nom.length * 3))
+      .style('fill', (d) => d.data.couleur);
+
+    node
+      .append('text')
+      .text((d) => d.data.nom)
+      .attr('dy', '0.2em')
+      .style('text-anchor', 'middle')
+      .style('font-family', (d) => d.data.police);
+
+    const links = treeData.links();
+
+    svg
+      .selectAll('.link')
+      .data(links)
+      .enter()
+      .append('path')
+      .attr('class', 'link')
+      .attr('d', (d) => `M${d.source.x},${d.source.y} L${d.target.x},${d.target.y}`)
+      .style('stroke', 'gray')
+      .style('stroke-width', 1)
+      .style('fill', 'none');
+  };
+
+  handleBubbleClick = (event, d) => {
+    const svg = d3.select(this.svgRef.current);
+    svg.node().appendChild(event.currentTarget);
+
+    d3.select(event.currentTarget)
+      .select('circle')
+      .style('stroke', 'black')
+      .style('stroke-width', 1);
+  };
+
+  AfficherBubbles = () => {
+    if (this.state.data) {
+      this.setState({ isBubbleChartVisible: true }, this.createBubbleChart);
+    }
+  };
+
+  render() {
+    const { isBubbleChartVisible, data } = this.state;
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div>
+          <h1>Bubbles</h1>
+          <input type="file" accept=".json" onChange={this.telechargerFichier} />
+          <button onClick={this.AfficherBubbles} disabled={!data}>
+            Afficher
+          </button>
+          {isBubbleChartVisible && <svg ref={this.svgRef}></svg>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BubbleChart;
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,25 +1,16 @@
-import ConfigurationModal from "./ConfigurationModal";
-
-
+import React from 'react';
+import BubbleChart from './BubbleChart';
 
 export default function MainContent() {
-    return (
-        <div className="w-4/5 p-6 overflow-auto bg-white">
-            {/* <div class="grid grid-cols-3 gap-6">
-                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-1 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-2 bg-white border border-gray-300"></div>
-                <div class="h-24 col-span-3 bg-white border border-gray-300"></div>
-            </div> */}
-
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam laborum quibusdam, dolorem mollitia sint, maxime nihil non quisquam eum ducimus? Sint suscipit ratione, enim omnis dolorem id eveniet porro.
-            </div>
-
-        </div>
-    )
+  return (
+    <div className="w-4/5 p-6 overflow-auto bg-white">
+      {
+        <BubbleChart />
+      }
+      
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam laborum quibusdam, dolorem mollitia sint, maxime nihil non quisquam eum ducimus? Sint suscipit ratione, enim omnis dolorem id eveniet porro.
+      </div>
+    </div>
+  );
 }
