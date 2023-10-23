53c68c1ca1124fcfc44bf3ce10c00f05b3278d79
geeksceo <oowashina@gmail.com>
2023-10-23 16:51:08 +0200

    Regraph integration and collapse fonctionnality integration

--- src/components/BubbleChart.jsx
+++ src/components/BubbleChart.jsx
@@ -1,123 +1,52 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
+import React, { Component, useRef, useState } from 'react';
 import '../App.css';
+import { GraphCanvas, useSelection} from 'reagraph';
+import data from '../data.json';
 
-class BubbleChart extends Component {
+export default function BubbleChart() {
 
-  // Permet de gerer la visibilité du graphe et les données
-  // du graphe.
-  state = {
-    data: null,
-    isBubbleChartVisible: false,
-  };
-
-  svgRef = React.createRef();
-  width = 900;
-  height = 600;
-  treeLayout = d3.tree().size([this.width, this.height]);
-  marginX = 50;
-  marginY = 50;
-
-  downloadFic = (event) => {
-    const file = event.target.files[0];
-    if (!file) return;
-
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      try {
-        this.setState({ data: JSON.parse(e.target.result) });
-      } catch (error) {
-        console.error('Erreur lors de la lecture du fichier JSON :', error);
-      }
-    };
-    reader.readAsText(file);
-  };
-
-  createBubbleChart = () => {
-    // Supprimez le contenu SVG existant s'il y en a un
-    d3.select(this.svgRef.current).selectAll('*').remove();
-
-    const { data } = this.state;
-    const svg = d3.select(this.svgRef.current)
-    .attr('width', this.width)
-    .attr('height', this.height);
-
-    const root = d3.hierarchy(data);
-    const treeData = this.treeLayout(root);
-
-    treeData.descendants().forEach((node) => {
-      // Ajoutez les marges aux coordonnées x et y
-      node.x += this.marginX;
-      node.y = node.y * 0.5 + this.marginY; // Pour réduire la longueur des liens
-    });
-
-    const node = svg
-      .selectAll('.node')
-      .data(treeData.descendants())
-      .enter()
-      .append('g')
-      .attr('class', 'node')
-      .attr('transform', (d) => `translate(${d.x},${d.y})`)
-      .on('click', this.handleBubbleClick);
-
-    node
-      .append('circle')
-      .attr('r', (d) => Math.max(15, d.data.nom.length * 3))
-      .style('fill', (d) => d.data.couleur);
-
-    node
-      .append('text')
-      .text((d) => d.data.nom)
-      .attr('dy', '0.2em')
-      .style('text-anchor', 'middle')
-      .style('font-family', (d) => d.data.police);
-
-    const links = treeData.links();
-
-    svg
-      .selectAll('.link')
-      .data(links)
-      .enter()
-      .append('path')
-      .attr('class', 'link')
-      .attr('d', (d) => `M${d.source.x},${d.source.y} L${d.target.x},${d.target.y}`)
-      .style('stroke', 'gray')
-      .style('stroke-width', 1)
-      .style('fill', 'none');
-  };
-
-  handleBubbleClick = (event, d) => {
-    const svg = d3.select(this.svgRef.current);
-    svg.node().appendChild(event.currentTarget);
-
-    d3.select(event.currentTarget)
-      .select('circle')
-      .style('stroke', 'black')
-      .style('stroke-width', 1);
-  };
-
-  showBubbles = () => {
-    if (this.state.data) {
-      this.setState({ isBubbleChartVisible: true }, this.createBubbleChart);
-    }
-  };
-
-  render() {
-    const { isBubbleChartVisible, data } = this.state;
-    return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <div>
-          <input type="file" accept=".json" onChange={this.downloadFic} />
+  const graphRef = useRef(null);
 
 
-          <button className="custom-styling flex-shrink-0 h-10 px-5 ml-2 text-sm font-medium border rounded-lg " onClick={this.showBubbles} disabled={!data}>
-              Afficher
-          </button>
-          {isBubbleChartVisible && <svg ref={this.svgRef}></svg>}
-        </div>
-      </div>
-    );
-  }
-}
+  const [collapsed, setCollapsed] = useState(['2']);
 
-export default BubbleChart;
+  const {
+    selections,
+    onNodeClick,
+    onCanvasClick
+  } = useSelection({
+    ref: graphRef,
+    nodes: data.nodes,
+    edges: data.edges
+  });
+
+  return  <div className='flex justify-center'>
+  <div className='relative w-full h-[800px] flex justify-center'>
+    {/* <GraphCanvas
+        nodes={data.nodes}
+        edges={data.edges}
+        // theme={theme}
+        sizingType="centrality"
+    /> */}
+
+    <GraphCanvas
+      draggable
+      ref={graphRef}
+      collapsedNodeIds={collapsed}
+      nodes={data.nodes}
+      edges={data.edges}
+      selections={selections}
+      onNodeClick={(node, props) => {
+        console.log(node.id.toString())
+        console.log(collapsed)
+        if(collapsed.includes(node.id.toString()))
+          setCollapsed(collapsed.filter(n => n !== node.id.toString()));
+        else
+          setCollapsed([...collapsed, node.id])
+
+      }}
+      onCanvasClick={onCanvasClick} />;
+  </div>
+</div>
+
+};
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,8 +3,9 @@ import BubbleChart from './BubbleChart';
 import CustomChart from './CustomChart';
 
 export default function MainContent() {
+
   return (
-    <div className="w-4/5 p-6 overflow-auto bg-white">
+    <div className="w-full p-6 overflow-auto bg-white">
         <BubbleChart />
       <div>
       </div>
